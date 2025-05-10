b92d283cfff6d531a2c92fc8d17c4a3d1fe083eb
introsent <161451906+introsent@users.noreply.github.com>
2025-05-10 17:19:15 +0200

    feat: bind prefab and unit type

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,15 +1,36 @@
+using System.Collections.Generic;
 using Unity.Properties;
 using UnityEngine;
 
+public enum UnitType
+{
+    Tank,
+    Melee,
+    Sniper
+}
+
+[System.Serializable]
+public struct UnitPrefabMapping
+{
+    public UnitType type;
+    public GameObject prefab;
+}
 public class SpawnManager : MonoBehaviour
 {
-    //public enum UnitType
-    //{
-    //    Tank,
-    //    Melee,
-    //    
-    //}
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public List<UnitPrefabMapping> _unitPrefabs = new List<UnitPrefabMapping>();
+    private Dictionary<UnitType, GameObject> _prefabMap;
+
+    private void Awake()
+    {
+        _prefabMap = new Dictionary<UnitType, GameObject>();
+        foreach (var mapping in _unitPrefabs)
+        {
+            if (mapping.prefab != null)
+            {
+                _prefabMap[mapping.type] = mapping.prefab;
+            }
+        }
+    }
     void Start()
     {
         
